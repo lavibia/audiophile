d6e713fc26eeeaacdd3b861daf4f019891cc7225
Lavinia Bianca <118028793+lavibia@users.noreply.github.com>
2024-09-10 00:49:53 +0300

    Fetch category products

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -13,7 +15,17 @@ module.exports = {
       title: 'Audiophile | audio gear',
       template: "src/index.html",
     }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/data.json',
+          to: path.resolve(__dirname, "dist"),
+
+        },
+      ],
+    }),
   ],
+
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
@@ -25,11 +37,26 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      }, {
+        test: /\.json$/i,
+        type: "asset/resource",
+      },
+
     ],
 
   },
-
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`
+      new JsonMinimizerPlugin(),
+    ],
+  },
 };
