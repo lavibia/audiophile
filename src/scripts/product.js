62de3cd5d406c7fd66fa859edb086139d26c0a9b
Lavinia Bianca <118028793+lavibia@users.noreply.github.com>
2024-09-30 18:17:36 +0300

    Fix quantity buttons

--- src/scripts/product.js
+++ src/scripts/product.js
@@ -9,8 +9,7 @@ window.onload = async function () {
  const title = urlParams.get('title');
  window.history.replaceState({}, '', `product.html?title=${title}`);
  if (title) {
-  let headerTitle = document.querySelector('.product__hero>#productTitle')
-  headerTitle.textContent = 'Test'
+
 
   // let list = await getCategoryProducts(category)
   // console.log(list);
@@ -21,3 +20,68 @@ window.onload = async function () {
   console.error('No product found');
  }
 };
+updateButtonStates();
+document.getElementById("go-back").addEventListener("click", () => {
+ history.back();
+});
+
+
+
+
+
+
+// Quantity management
+document.querySelector(".quantity").addEventListener("click", (Event) => {
+ if (Event.target.classList.contains("minus")) {
+  decreaseQuantityDOM();
+ } else if (Event.target.classList.contains("plus")) {
+  increaseQuantityDOM();
+ }
+})
+document.querySelector(".quantity>.input-box").addEventListener("focusout", handleQuantityChange);
+
+function decreaseQuantityDOM() {
+
+ const inputBox = document.querySelector(".quantity>.input-box")
+ let value = parseInt(inputBox.value);
+ // value = isNaN(value) ? 1 : Math.max(value - 1, 1);
+ value = isNaN(value) ? 1 : value - 1;
+ inputBox.value = value;
+ updateButtonStates();
+ handleQuantityChange();
+}
+function increaseQuantityDOM() {
+ const inputBox = document.querySelector(".quantity>.input-box")
+ let value = parseInt(inputBox.value);
+ // value = isNaN(value) ? 1 : Math.min(value + 1, parseInt(inputBox.max));
+ value = isNaN(value) ? 1 : value + 1;
+ inputBox.value = value;
+ updateButtonStates();
+ handleQuantityChange();
+}
+
+function handleQuantityChange() {
+ const inputBox = document.querySelector(".quantity>.input-box")
+ let value = parseInt(inputBox.value);
+
+
+ value = isNaN(value) ? 1 : value;
+
+ if (value > parseInt(inputBox.max))
+  value = parseInt(inputBox.max);
+ else if (value < parseInt(inputBox.min))
+  value = parseInt(inputBox.min)
+
+
+ // Execute your code here based on the updated quantity value
+ inputBox.value = value;
+ console.log("Quantity changed:", value);
+}
+function updateButtonStates() {
+ const inputBox = document.querySelector(".quantity>.input-box");
+ const minusBtn = document.querySelector(".quantity>.minus");
+ const plusBtn = document.querySelector(".quantity>.plus");
+ const value = parseInt(inputBox.value);
+ minusBtn.disabled = value <= 1;
+ plusBtn.disabled = value >= parseInt(inputBox.max);
+}
