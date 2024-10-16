b8a21029801eac0d51d85389f570788643d4e546
Lavinia Bianca <118028793+lavibia@users.noreply.github.com>
2024-10-16 13:30:44 +0300

    Load you may also like section on product details

--- src/scripts/DOMProduct.js
+++ src/scripts/DOMProduct.js
@@ -1,12 +1,12 @@
 export function renderProductDetails(productInfo, wrapper) {
- // wrapper is product details
- // for (let i = 0; i < productInfo.length; i++) {
- let descriptionDOM = document.createElement('div');
- descriptionDOM.classList.add('product__description');
- // img section
- let pictureDOM = document.createElement('picture');
+  // wrapper is product details
+  // for (let i = 0; i < productInfo.length; i++) {
+  let descriptionDOM = document.createElement('div');
+  descriptionDOM.classList.add('product__description');
+  // img section
+  let pictureDOM = document.createElement('picture');
 
- pictureDOM.innerHTML = `
+  pictureDOM.innerHTML = `
             <source
               media="(max-width: 35.74rem)"
               srcset=".${productInfo.image.mobile}"
@@ -27,37 +27,37 @@ export function renderProductDetails(productInfo, wrapper) {
             />
             <img src=".${productInfo.image.mobile}" alt="" />
   `
- descriptionDOM.appendChild(pictureDOM);
- // product desc info
- let wrapperText = document.createElement('div');
- wrapperText.classList.add('product__description__text')
+  descriptionDOM.appendChild(pictureDOM);
+  // product desc info
+  let wrapperText = document.createElement('div');
+  wrapperText.classList.add('product__description__text')
 
- // new product text
- if (productInfo.new === true) {
-  let newProdDOM = document.createElement('div');
-  newProdDOM.classList.add('color__accent', 'overline');
-  newProdDOM.textContent = 'New Product';
-  wrapperText.appendChild(newProdDOM);
- }
+  // new product text
+  if (productInfo.new === true) {
+    let newProdDOM = document.createElement('div');
+    newProdDOM.classList.add('color__accent', 'overline');
+    newProdDOM.textContent = 'New Product';
+    wrapperText.appendChild(newProdDOM);
+  }
 
- //title
- let titleDOM = document.createElement('h1');
- titleDOM.classList.add('h2');
- titleDOM.textContent = `${productInfo.name}`;
- wrapperText.appendChild(titleDOM);
+  //title
+  let titleDOM = document.createElement('h1');
+  titleDOM.classList.add('h2');
+  titleDOM.textContent = `${productInfo.name}`;
+  wrapperText.appendChild(titleDOM);
 
- // description
+  // description
 
- let descDOM = document.createElement('p');
- descDOM.textContent = productInfo.description;
- wrapperText.appendChild(descDOM);
+  let descDOM = document.createElement('p');
+  descDOM.textContent = productInfo.description;
+  wrapperText.appendChild(descDOM);
 
 
- // form quantity
- let formDOM = document.createElement('form');
- formDOM.action = "#";
- formDOM.classList.add("product-quantity");
- formDOM.innerHTML = `<label for="quantity-value" class="visibily__hidden"
+  // form quantity
+  let formDOM = document.createElement('form');
+  formDOM.action = "#";
+  formDOM.classList.add("product-quantity");
+  formDOM.innerHTML = `<label for="quantity-value" class="visibily__hidden"
               >Quantity</label
             >
             <div class="quantity">
@@ -81,59 +81,56 @@ export function renderProductDetails(productInfo, wrapper) {
               </button>
             </div>
             <button type="submit">Add to cart</button>`
+  wrapperText.appendChild(formDOM);
+  descriptionDOM.appendChild(wrapperText);
+
+  wrapper.appendChild(descriptionDOM);
+
+  //features
+  let featuresWrapper = document.createElement('div');
+  featuresWrapper.classList.add('product__features');
+  let featTitle = document.createElement('h2');
+  featTitle.classList.add('h3');
+  featTitle.textContent = 'Features';
+
+  let featText = document.createElement('p');
+  featText.textContent = productInfo.features;
 
 
+  featuresWrapper.appendChild(featTitle)
+  featuresWrapper.appendChild(featText);
 
- wrapperText.appendChild(formDOM);
- descriptionDOM.appendChild(wrapperText);
-
- wrapper.appendChild(descriptionDOM);
-
- //features
- let featuresWrapper = document.createElement('div');
- featuresWrapper.classList.add('product__features');
- let featTitle = document.createElement('h2');
- featTitle.classList.add('h3');
- featTitle.textContent = 'Features';
-
- let featText = document.createElement('p');
- featText.textContent = productInfo.features;
+  wrapper.appendChild(featuresWrapper);
 
 
- featuresWrapper.appendChild(featTitle)
- featuresWrapper.appendChild(featText);
+  // includes
 
- wrapper.appendChild(featuresWrapper);
+  let includesWrapperDOM = document.createElement('div');
+  let includeTitle = document.createElement('h2');
+  includeTitle.classList.add('h3');
 
+  includeTitle.textContent = 'in the box';
+  includesWrapperDOM.appendChild(includeTitle);
 
- // includes
-
- let includesWrapperDOM = document.createElement('div');
- let includeTitle = document.createElement('h2');
- includeTitle.classList.add('h3');
-
- includeTitle.textContent = 'in the box';
- includesWrapperDOM.appendChild(includeTitle);
-
- let includeList = document.createElement('ul');
- for (let i = 0; i < productInfo.includes.length; i++) {
-  let itemDOM = document.createElement('li');
-  itemDOM.innerHTML = `
+  let includeList = document.createElement('ul');
+  for (let i = 0; i < productInfo.includes.length; i++) {
+    let itemDOM = document.createElement('li');
+    itemDOM.innerHTML = `
 <span class="subtitle include__quantity">${productInfo.includes[i].quantity}x</span>${productInfo.includes[i].item}
 `
-  includeList.appendChild(itemDOM);
- }
+    includeList.appendChild(itemDOM);
+  }
 
- includesWrapperDOM.appendChild(includeList);
+  includesWrapperDOM.appendChild(includeList);
 
- wrapper.appendChild(includesWrapperDOM);
+  wrapper.appendChild(includesWrapperDOM);
 
- // galery product
- let orderGallery = ['first', 'second', 'third']
- let galleryDOM = document.createElement('div');
- for (let i = 0; i < orderGallery.length; i++) {
-  let galleryPic = document.createElement('picture');
-  galleryPic.innerHTML = `<source
+  // galery product
+  let orderGallery = ['first', 'second', 'third']
+  let galleryDOM = document.createElement('div');
+  for (let i = 0; i < orderGallery.length; i++) {
+    let galleryPic = document.createElement('picture');
+    galleryPic.innerHTML = `<source
               media="(max-width: 35.74rem)"
               srcset=".${productInfo.gallery[orderGallery[i]].mobile}"
               width="100%"
@@ -153,11 +150,68 @@ export function renderProductDetails(productInfo, wrapper) {
             />
             <img src=".${productInfo.gallery[orderGallery[i]].mobile}" alt="" />
   `;
-  galleryDOM.appendChild(galleryPic);
+    galleryDOM.appendChild(galleryPic);
+  }
+  wrapper.appendChild(galleryDOM);
 
- }
+  //you may also like
 
- wrapper.appendChild(galleryDOM);
+  let othersLikeDOM = document.createElement('div');
+  //title
+  let othersLikeTitle = document.createElement('h2');
+  othersLikeTitle.classList.add('h3');
+  othersLikeTitle.textContent = "You may also like";
+  othersLikeDOM.appendChild(othersLikeTitle);
+
+
+  //others list
+  let othersList = document.createElement('ul');
+  for (let i = 0; i < productInfo.others.length; i++) {
+    let otherItem = document.createElement('li');
+
+    //image for item you may like
+    let othersLikeImg = document.createElement('picture');
+    othersLikeImg.innerHTML = `<source
+              media="(max-width: 35.74rem)"
+              srcset=".${productInfo.others[i].image.mobile}"
+              width="100%"
+              height="auto"
+            />
+            <source
+              media="(max-width: 62.4rem)"
+              srcset='.${productInfo.others[i].image.tablet}'
+              width="100%"
+              height="auto"
+            />
+            <source
+              media="(min-width: 62.4rem)"
+              srcset='.${productInfo.others[i].image.desktop}'
+              width="100%"
+              height="auto"
+            />
+            <img src=".${productInfo.others[i].image.mobile}" alt="" />`
+    otherItem.appendChild(othersLikeImg);
+    //name for item you may like
+    let otherLikeName = document.createElement('h3');
+    otherLikeName.classList.add('h5');
+    otherLikeName.textContent = productInfo.others[i].name;
+    otherItem.appendChild(otherLikeName);
+
+
+    // see link for item you may like
+    let otherLikeBtn = document.createElement('a');
+    otherLikeBtn.classList.add('btn__pink');
+    otherLikeBtn.classList.add('subtitle');
+    otherLikeBtn.setAttribute('href', `product.html?title=${productInfo.others[i].slug}`);
+    otherLikeBtn.textContent = 'See Product';
+    otherItem.appendChild(otherLikeBtn);
+
+    //add item to list
+    othersList.appendChild(otherItem);
+  }
+  othersLikeDOM.appendChild(othersList);
+  wrapper.appendChild(othersLikeDOM);
+
 
 
 
